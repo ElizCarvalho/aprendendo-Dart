2e2cc085ef7fad5892e486f6c80d9d8c32a544cc
Elizabeth Carvalho <elizabethcarvalh0@yahoo.com>
2019-03-05 12:44:09 -0300

    Functions

--- 08-functions.dart
+++ 08-functions.dart
@@ -1,7 +1,19 @@
 void main(){
   hello("Eliz");
+  hello2('Eliz', sobrenome:'Silva');
+  hello3('Eliz', 32,'Souza');
 }
 
 void hello(String nome){
   print("Ola $nome");
+}
+
+//paremetros opcionias nomeados entre {}
+void hello2(String nome, {String sobrenome = 'Carvalho', int idade = 0}){
+  print("Ola $nome $sobrenome. Sua idade é $idade");
+}
+
+//parametros opcionias posicionais entre []
+void hello3(String nome, [int idade = 0, String sobrenome = 'Carvalho']){
+  print("Ola $nome $sobrenome. Sua idade é $idade");
 }
